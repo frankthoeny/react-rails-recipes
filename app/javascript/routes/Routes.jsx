85eb3f2d4b8c43d34987d3ae348e25a17249b2b8
Anna Mendoza <annamendoza.curve@gmail.com>
2021-08-15 00:51:10 -0700

    got Step 7 done, need to review it one more time to see what is nuanced and what is clunky

--- app/javascript/routes/Routes.jsx
+++ app/javascript/routes/Routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import Recipes from "../components/Recipes";
 import About from "../components/About";
+import Recipe from "../components/Recipe";
 
 
 export default (
@@ -11,6 +12,7 @@ export default (
       <Route path="/" exact component={Home} />
       <Route path="/recipes" exact component={Recipes} />
       <Route path="/about" exact component={About} />
+      <Route path="/recipe/:id" exact component={Recipe} />
     </Switch>
   </Router>
 );
